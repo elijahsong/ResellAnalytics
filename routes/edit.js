73ddf97122807bce26b4f827d2cadbe38432e248
Elijah Song <elijahjsong@gmail.com>
2022-09-01 12:00:00 -0500

    test 2

--- routes/edit.js
+++ routes/edit.js
@@ -65,13 +65,13 @@ router
           const retailPrice = jsonObject.retailPrice;
           const thumbnailURL = jsonObject.thumbnail;
           const resellLinkJSON = jsonObject.resellLinks;
-          const resellLink_StockX = resellLinkJSON.stockX;
+          const resellLink_StockX = `"${resellLinkJSON.stockX}"`;
           
           console.log(`StockX resell price is: ${resellPrice_StockX}`);
           console.log(`Current retail price is: ${retailPrice}`);
           console.log(`Resell link: ${resellLink_StockX}`);
           console.log(`Thumbnail URL: ${thumbnailURL}`);
-          
+
           resolve({
             resellPrice_StockX: resellPrice_StockX,
             retailPrice: retailPrice,
