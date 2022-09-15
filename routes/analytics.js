e979bf29ef90105f611481d8a580ec688035dd20
Elijah Song <elijahjsong@gmail.com>
2022-09-14 19:32:41 -0500

    sql queries

--- routes/analytics.js
+++ routes/analytics.js
@@ -23,15 +23,39 @@ router
 router.get('/results', function(req, res,next) {
   // These will require more involved subqueries and aggregation
   // Queries to get results BETWEEN two dates
-  // Add up net profit, coalescing null values
-  // Add up revenue, coalescing necessary or not?
-  // Find average profit margin = profit / revenue
-  // Average time to sale: 
+  // 1. Average price bought
+  // 2. Average price sold
+  // 3. Average profit per shoe
+  // 4. Net (total) profit
+  // 4. Total Revenue
+  // 5. Profit margin
+  // 7. Average time to sale
 
   var from = req.cookies["from"];
   var to = req.cookies["to"];
   console.log(from);
   console.log(to);
+
+  var avg_bought = `SELECT ROUND(AVG(price), 2) FROM Purchases WHERE date BETWEEN ${from} AND ${to};`
+  var avg_sold = `SELECT ROUND(AVG(price), 2) FROM Sales WHERE date BETWEEN ${from} AND ${to};`
+  var avg_profit = `SELECT ROUND(AVG(s.price - p.price - COALESCE(s.extra_fees, 0.00)), 2)
+  FROM Purchases p JOIN Sales s ON p.id = s.sales_id
+  WHERE (p.date BETWEEN ${from} AND ${to})
+  AND (s.date BETWEEN ${from} AND ${to})`
+
+  var net_profit = `SELECT ROUND(SUM(s.price - p.price - COALESCE(s.extra_fees, 0.00)), 2)
+  FROM Purchases p JOIN Sales s ON p.id = s.sales_id
+  WHERE (p.date BETWEEN ${from} AND ${to})
+  AND (s.date BETWEEN ${from} AND ${to})`
+
+  var revenue = `SELECT ROUND(SUM(price), 2)
+  FROM Purchases
+  WHERE date BETWEEN ${from} AND ${to}`
+
+  var margin = `` // value of net_profit query divided by revenue times 100
+  var cycle = `SELECT ROUND(AVG(DATEDIFF(${to}, ${from})), 2)
+  FROM Purchases p JOIN Sales s ON p.id = s.sales_id`
+
   res.render('analyticsresults', { title: 'Analytics Results' });
 });
 
